8facc989125fc2f1776d8da729218ebbc8ed9346
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-23 16:05:08 +0000

    minor clean up of comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,20 +3,15 @@ $(document).ready(function() {
   console.log('hellllllllooooooo');
 
   $(text$).on('focus', function( event ) {
-    //event.target.style.background = 'pink';  
     console.log('inside');
   });
 
 
   $(text$).on('blur', function( event ) {
-    //event.target.style.background = ''; 
     console.log('outside');   
   });
 
   $(text$).on('keyup', function( event ) {
-    //event.target.style.background = ''; 
-    //console.log(event.target.value);
-    //console.log(this);
 
     var counter = event.target.value.length;
 
@@ -27,47 +22,8 @@ $(document).ready(function() {
       console.log('allowed letters')
     }
     else {
-      //$(text$).css('color','red')  
-      let countertxt = $('#textcounter').text(140 - counter).css('color','red') 
-      //return countertxt;
+      let countertxt = $('#textcounter').text(140 - counter).css('color','red');
       console.log('toomanyletters')
-
-      //let countertxt = document.getElementById("textcounter").innerHTML = 140 - counter;
-      //countertxt.style.color = #FF0000;
-      //this.style.color = #FF0000;
-      //document.getElementById("textcounter")
-      //.style.color = #FF0000;
     }
-
-    
-    
-    // //console.log(counter);
-    // let countertxt = document.getElementById("textcounter").innerHTML = counter
-  
-    // //console.log('a'); 
-  });
-
-
-});
-
-
-
-// function textCounter(field, cnt, maxlimit) {         
-//   var cntfield = document.getElementById("textcounter")   
-//   if (field.value.length > maxlimit) // if too long...trim it!
-//     field.value = field.value.substring(0, maxlimit);
-//     // otherwise, update 'characters left' counter
-//   else
-//     cntfield.value = maxlimit - field.value.length;
-// }
-
-
-
-
-
-
-// document.addEventListener("dblclick", (event) => {
-//   console.log(event);
-//   "clientX: " + event.clientX +
-//   " - clientY: " + event.clientY;
-// });
+  })
+})
