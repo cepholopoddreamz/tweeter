5e64ac097d079a200983698e01d2b17d7af0463a
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-24 01:03:14 +0000

    adding dynamic tweets part 1 and 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,97 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+var tweety_the_tweet = {
+  user: {
+    name: 'Newton',
+    avatars: {
+      small:   'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png',
+      regular: 'https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png',
+      large:   'https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png'
+    },
+    handle: '@SirIsaac'
+  },
+  content: {
+    text: 'If I have seen further it is by standing on the shoulders of giants'
+  },
+  created_at: 1461116232227
+};
+
+
+
+function createTweetElement (tweet_passed_as_parameter){
+// var newPetElement = `<p>${item.name}</p>`
+    // Apend this to the page
+    // $petsContainer.append(newPetElement)
+    var testkey = tweety_the_tweet.content.text;
+
+
+    
+    
+  //var text$ = document.getElementById('.tweetresponse');
+  $ ('main.container').append("<h1>testing</h1>");
+  //$ (text$).append("<p>testing</p>");
+  $('<article>').append(`<p>testing ${testkey}</p>`);
+
+
+  //$('<article>').append($tweet);
+
+}
+
+$(document).ready(function() {
+
+createTweetElement(tweety_the_tweet);
+
+});
+// var bob = 'img1.jpg'
+
+// $('main').append('<img src="'+bob+''">')
+// $('main').append(`<img src="${bob}">`)
+
+// var $tweet = $("<article>").addClass("tweet");
+
+
+
+
+
+//when i use the function: 
+/////$(document).ready(function() {
+
+//$(document).ready(function() {?
+
+
+// function reqListener () {
+//   $articles = document.querySelector("<articles>") /// does this need to be .tweetresponse?
+//   console.log(this.responseText)
+//   $articles.innerHTML = this.responseText
+// }
+
+// function createTweetElement (){
+
+
+// }
+
+//? $(document).ready(function() {
+ // var text$ = document.getElementById('hittest');
+ 
+ 
+ //var $tweet = $(text$).addClass("tweet");
+
+
+
+
+// {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": {
+//       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+//       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+//       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+//     },
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+// }
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,30 @@ $(document).ready(function() {
   var text$ = document.getElementById('hittest');
   console.log('hellllllllooooooo');
 
+  $("article.box2").on('focus', function( event ) {
+    console.log('inside');
+  });
+
+  // $("section.box2").hover(function(){
+  //   $(this).fadeTo(1000, 0.4);
+  // });
+
+  // $("section.box2").ready(function() {
+  //   $(this).css({opacity: 0.4}, 10);
+  // })
+
+  $("article.box2").hover(function(){
+    $(this).animate({opacity: 1.0}, 300);
+  });
+  //$(selector).fadeTo(speed,opacity,easing,callback)
+
+  $("article.box2").mouseleave(function(){
+    $(this).animate({opacity: 0.4}, 300);
+  });
+
+
+
+
   $(text$).on('focus', function( event ) {
     console.log('inside');
   });
