b3a6beea1e2b606ea3e6347479839a15d0fb9f6f
cepholopoddreamz <leslie.plumb@gmail.com>
2019-01-27 17:59:55 +0000

    added autofocus to text area box after compose button clicked

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
   console.log('hellllllllooooooo');
 
   $("article.box2").on('focus', function( event ) {
-    console.log('inside');
+    //console.log('inside');
   });
 
   $("article.box2").hover(function(){
+    console.log('hovering')
     $('#titlespace2').animate({opacity: 1.0}, 300);
     $('#tweetfooter').animate({opacity: 1.0}, 300);
     $('h2.tweetresponse').animate({opacity: 1.0}, 300);
@@ -16,16 +17,26 @@ $(document).ready(function() {
   });
 
   $("article.box2").mouseleave(function(){
+    console.log('mouse leave')
     $('#titlespace2').animate({opacity: 0.4}, 300);
     $('#tweetfooter').animate({opacity: 0.4}, 300);
     $('h2.tweetresponse').animate({opacity: 1.0}, 300);
   });
 
 
+  // $("span.composebt button").hover(function(){
+  //   $(this).animate({opacity: 1.0}, 300);
+  // });
+
+  // $("span.composebt button").hover(function(){
+  //   $(this).mouseleave({opacity: 0.7}, 300);
+  // });
+
+
 
 
   $(text$).on('focus', function( event ) {
-    console.log('inside');
+    //console.log('inside');
   });
 
 
@@ -35,7 +46,7 @@ $(document).ready(function() {
 
   $(text$).on('keyup', function( event ) {
 
-    var counter = event.target.value.length;
+    var counter = (event.target.value.length);
     if (counter === 0 || typeof counter === 'undefined'){
       alert(' you need to enter some content');
     }
@@ -44,12 +55,18 @@ $(document).ready(function() {
       $('#textcounter').text(counter);
       //return countertxt;
       console.log('allowed letters')
-      
     }
     else {
-      let countertxt = $('#textcounter').text(140 - counter).css('color','red');
+      let countertxt = $('#textcounter').text(139 - counter).css('color','red');
       //console.log('toomanyletters')
       alert(' tweet content is too long');
     }
   })
+
+  $( "#composebt" ).click(function(event) {
+    $( "section.box" ).slideToggle( "slow", function() {
+      $(this).find(".tweettextarea").focus().select()  
+    });
+  });
+
 })
